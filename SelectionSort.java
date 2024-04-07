918f6b0edeaddf1195409e9a1e254d5a818bc371
Srikanth <srikanthsailada1996@gmail.com>
2024-04-07 14:52:46 +0530

    Adding Selection sort code

---
+++ SelectionSort.java
@@ -0,0 +1,34 @@
+public class SelectionSort {
+    public static void main(String[] args)
+    {
+        int nums[] = {6,5,2,8,9,4};
+        int temp = 0;
+        int min_index = -1;
+        System.out.println("Before Swapping: ");
+        for(int num: nums)
+        {
+            System.out.print(num+" ");
+        }
+
+        for(int i=0; i<nums.length-1; i++)
+        {
+            min_index = i;
+            for(int j=i+1; j<nums.length; j++)
+            {
+                if(nums[min_index] > nums[j]) {
+                    min_index = j;
+                }
+            }
+
+            temp = nums[min_index];
+            nums[min_index] = nums[i];
+            nums[i] = temp;
+        }
+
+        System.out.println("After Swapping: ");
+        for(int num: nums)
+        {
+            System.out.print(num+" ");
+        }
+    }
+}
